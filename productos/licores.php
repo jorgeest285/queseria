9e3f72a2f0040a06856dd7c368bcf2b93b6e4a8b
Jorge <jorgeestrada285.je@gmail.com>
2025-01-18 19:20:08 -0600

    Modificación de los titulos de cada producto

--- productos/licores.php
+++ productos/licores.php
@@ -15,7 +15,7 @@ $stmt = $pdo->query($sql);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Queseria Báez | Licores</title>
+    <title>Queseria el Veracruzano | Licores</title>
     <link rel="shortcut icon" href="img/logo.png" type="image/x-icon">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
     <script defer src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" integrity="sha384-rOA1PnstxnOBLzCLMcre8ybwbTmemjzdNlILg8O7z1lUkLXozs4DHonlDtnE7fpc" crossorigin="anonymous"></script>
--- registro.php
+++ registro.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro de Usuario | Queseria el veracruzano</title>
+    <title>Registro de Usuario | Queseria el Veracruzano</title>
     <link rel="shortcut icon" href="img/logos/logo.png" type="image/x-icon">
     <!-- Incluir SweetAlert -->
     <link rel="stylesheet" href="assets/sweetalert2.min.css">
